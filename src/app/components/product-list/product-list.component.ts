d02036f47c1ba5d45b7815e8f60560a3a0507297
geetesh <gtsh321@gmail.com>
2025-02-21 19:53:14 +0530

    added the routes in the app-routing-module.ts, placed the routerLink and routerOutlet in app-component.html , added the logic in product-service.ts to access the product by category , updated the product-list.component.ts to get the id from the route

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -11,14 +12,31 @@ import { Product } from '../../common/product';
 export class ProductListComponent {
 
   products: Product[] = [];
-  constructor(private productService: ProductService) {
+  currentCategoryId:number=1;
+  constructor(private productService: ProductService, private route:ActivatedRoute) {
   }
 
   ngOnInit(): void {
+   this.route.paramMap.subscribe(()=>{
     this.listProducts();
+   }) 
   }
+
   listProducts() {
-    this.productService.getProductList().subscribe(
+    //check if "id" parameter is available
+    const hasCategoryId: boolean=this.route.snapshot.paramMap.has('id');
+    
+    if(hasCategoryId){
+     //get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else
+    {
+      //not category id available ... default to category id 1
+      this.currentCategoryId=1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,12 +8,13 @@ import { Product } from '../common/product';
 })
 export class ProductService {
   
-  private baseUrl = 'http://localhost:8079/api/products?size=100'
+  private baseUrl = 'http://localhost:8079/api/products'
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId:number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
