0d0fa799f5d8fc23be4d4885310dc40a2aaa98b9
geetesh <gtsh321@gmail.com>
2025-02-27 17:22:29 +0530

    added a button for adding the product in the cart from product-details.component and in the ts added the corresponding method to call the cartService for the added product

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -10,7 +12,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent {
  product!:Product;
- constructor(private productService:ProductService,private route:ActivatedRoute){
+ constructor(private productService:ProductService,
+  private cartService:CartService,
+  private route:ActivatedRoute){
  }
  ngOnInit():void{
     this.route.paramMap.subscribe(()=>{
@@ -27,5 +31,11 @@ export class ProductDetailsComponent {
       this.product=data;
     }
    )
+  }
+
+  addToCart(){
+     console.log(`Adding to cart: ${this.product.name},${this.product.unitPrice}`);
+     let theCartItem = new CartItem(this.product);
+     this.cartService.addToCart(theCartItem);
   }
 }
