ce39908b72343cd167958d346c155dfb5df616b5
geetesh <gtsh321@gmail.com>
2025-04-20 13:39:07 +0530

    created a reactive form with formGroup checkoutFormGroup with having various nested formGroup in it {customer,shippingAddress, billingAddress, creditCard}

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -8,21 +8,60 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CheckoutComponent implements OnInit {
     checkoutFormGroup!:FormGroup;
+    
+    totalPrice:number=0;
+    totalQuantity:number=0;
 
     constructor(private formBuilder:FormBuilder){
     }
 
   ngOnInit(): void {
    
-    this.checkoutFormGroup = this.formBuilder.group({
+    this.checkoutFormGroup = this.formBuilder.group
+    ({
+
       customer:this.formBuilder.group({
         firstName:[''],
         lastName:[''],
         email:['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street:[''],
+        city:[''],
+        state:[''],
+        country:[''],
+        zipCode:['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street:[''],
+        city:[''],
+        state:[''],
+        country:[''],
+        zipCode:['']
+      }),
+      creditCard: this.formBuilder.group({
+        creditCard:[''],
+        nameOnCard:[''],
+        cardNumber:[''],
+        securityCode:[''],
+        expirationMonth:[''],
+        expirationYear:['']
       })
+      
     })
   }
-
+  
+  copyShippingAddressToBillingAddress(event:any)
+  {
+      if(event.target.checked)
+      {
+         this.checkoutFormGroup.controls['billingAddress'].setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+      }
+      else
+      {
+        this.checkoutFormGroup.controls['billingAddress'].reset();
+      }
+  }
   onSubmit()
   {
     console.log("Handling the submit button");
